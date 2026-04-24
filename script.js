f93b79669a35b5358043b15a6dd55c0ced09d257
.\.git\Camilo Andres Yela Ome <est.camilo.yela@unimilitar.edu.co>
2026-04-24 09:39:39 -0500

    Inicializacion de los codigos

---
+++ script.js
@@ -0,0 +1,7 @@
+JavaScript
+/**
+ * Universidad - Facultad de Ingeniería
+ * Asignatura: Introducción a la Computación Gráfica
+ * * Estudiante: ________________
+ * * Tarea: Implementar los algoritmos de rasterización manual.
+ */
