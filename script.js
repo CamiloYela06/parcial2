355aecaea573dddaf7ba5b9f744911611606e18e
.\.git\Camilo Andres Yela Ome <est.camilo.yela@unimilitar.edu.co>
2026-04-24 10:01:56 -0500

    Se finalizan las funciones que dibujan los poligonos regulares

--- script.js
+++ script.js
@@ -5,40 +5,42 @@
  * * Tarea: Implementar los algoritmos de rasterización manual.
  */
 
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
 // Función de apoyo para dibujar un píxel individual
 function drawPixel(ctx, x, y, color = "#000000") {
     ctx.fillStyle = color;
     ctx.fillRect(Math.floor(x), Math.floor(y), 1, 1);
 }
 
-/**
- * Implementación del algoritmo de Bresenham para líneas.
- * @param {number} x0, y0 - Coordenadas iniciales
- * @param {number} x1, y1 - Coordenadas finales
- * @returns {void}
- */
-function bresenhamLine(x0, y0, x1, y1) {
+//Implementación del algoritmo de Bresenham para líneas.
+function bresenhamLine(x0, y0, x1, y1, color ="#000000") {
     // Desarrollo del estudiante
     let dx = Math.abs(x1 - x0);
     let dy = Math.abs(y1 - y0);
 
-    let sx = (x0 < x1) ? 1 : -1;
-    let sy = (y0 < y1) ? 1 : -1;
+    let sx = (x0 < x1) ? 1 : -1; // dirección en X
+    let sy = (y0 < y1) ? 1 : -1; // dirección en Y
 
     let err = dx - dy;
 
     while (true) {
-        drawPixel(x0, y0);
 
+        drawPixel(ctx, x0, y0, color);
+
+        // condición de parada
         if (x0 === x1 && y0 === y1) break;
 
         let e2 = 2 * err;
 
+        // movimiento en X
         if (e2 > -dy) {
             err -= dy;
             x0 += sx;
         }
 
+        // movimiento en Y
         if (e2 < dx) {
             err += dx;
             y0 += sy;
@@ -46,13 +48,39 @@ function bresenhamLine(x0, y0, x1, y1) {
     }
 }
 
-/**
- * Calcula los vértices de un polígono regular.
- * @param {number} centerX, centerY - Centro
- * @param {number} sides - Número de lados
- * @param {number} radius - Radio
- * @returns {Array} Arreglo de objetos {x, y}
- */
+//Calcula los vértices de un polígono regular.
+
 function getPolygonVertices(centerX, centerY, sides, radius) {
     // Desarrollo del estudiante (Uso de Math.sin/Math.cos y retorno de datos)
+    let vertices = [];
+
+    for (let i = 0; i < sides; i++) {
+
+        let angle = (2 * Math.PI * i) / sides;
+
+        let x = centerX + radius * Math.cos(angle);
+        let y = centerY + radius * Math.sin(angle);
+
+        vertices.push({
+            x: Math.round(x),
+            y: Math.round(y)
+        });
+    }
+
+    return vertices;
 }
+
+/**
+ * Dibuja el polígono conectando los vértices
+ */
+function drawPolygon(vertices, color = "#000000") {
+
+    for (let i = 0; i < vertices.length; i++) {
+
+        let p1 = vertices[i];
+        let p2 = vertices[(i + 1) % vertices.length];
+
+        bresenhamLine(p1.x, p1.y, p2.x, p2.y, color);
+    }
+}
+
